f9244fb72f06c385982694712b0f341ec26071fd
Meghai Choudhury <meghaic03@gmail.com>
2025-07-23 13:12:34 -0700

    finished 1.3

--- src/scene/triangle.cpp
+++ src/scene/triangle.cpp
@@ -2,6 +2,7 @@
 
 #include "CGL/CGL.h"
 #include "GL/glew.h"
+#include "vector3D.h"
 
 namespace CGL {
 namespace SceneObjects {
@@ -28,19 +29,68 @@ bool Triangle::has_intersection(const Ray &r) const {
   // function records the "intersection" while this function only tests whether
   // there is a intersection.
 
+  Vector3D e1 = p2 - p1;
+  Vector3D e2 = p3 - p1;
+  Vector3D ray_cross_e2 = cross(r.d, e2);
+  double det = dot(e1, ray_cross_e2);
+  if (det < 1e-6 && det > -1e-6) {
+    return false; // Ray is parallel to the triangle
+  }
+  Vector3D s = r.o - p1;   //distance from p1 to ray origin
+  double u = (1.0/det) * dot(s, ray_cross_e2);   //u parameter
 
-  return true;
+  Vector3D q = cross(s, e1); //vector perpendicular to both s and edge1
+  double v = (1.0/det) * dot(r.d, q);
 
+  double w = 1 - u - v;
+
+  if(u>1.0 || v>1.0 || u<0.0 || v<0.0 || w<0.0 || w>1.0) {
+    return false;
+  }
+
+  double t = (1.0/det) * dot(e2, q);
+  if (t >= r.min_t && t <= r.max_t) {  // Check if intersection is within valid t range
+    return true;
+  }else{ return false;}
 }
 
 bool Triangle::intersect(const Ray &r, Intersection *isect) const {
   // Part 1, Task 3:
   // implement ray-triangle intersection. When an intersection takes
   // place, the Intersection data should be updated accordingly
+  Vector3D e1 = p2 - p1;
+  Vector3D e2 = p3 - p1;
+  Vector3D ray_cross_e2 = cross(r.d, e2);
+  double det = dot(e1, ray_cross_e2);
+  if (det < 1e-6 && det > -1e-6) {
+    return false; // Ray is parallel to the triangle
+  }
+  Vector3D s = r.o - p1;   //distance from p1 to ray origin
+  double u = (1.0/det) * dot(s, ray_cross_e2);   //u parameter
 
+  Vector3D q = cross(s, e1); //vector perpendicular to both s and edge1
+  double v = (1.0/det) * dot(r.d, q);
 
-  return true;
+  double w = 1.0 - u - v;
 
+  if(u>1.0 || v>1.0 || u<0.0 || v<0.0 || w<0.0 || w>1.0) {
+    return false;
+  }
+
+  double t = (1.0/det) * dot(e2, q);
+  if (t >= r.min_t && t <= r.max_t) {
+    // interpolate normal using barycentric coordinates with vertex normals
+    Vector3D interpolated_normal = (w * n1) + (u * n2) + (v * n3);
+    interpolated_normal = interpolated_normal.unit();
+    
+    // populate its input Intersection *isect structure
+    isect->t = t;
+    isect->n = interpolated_normal;
+    isect->bsdf = bsdf;
+    isect->primitive = this;    
+
+    return true;
+  }else{return false;}
 
 }
 
